8cfa293afb1e897c30d5174babe0754c1726e96e
Connor Guy Meehan <connorgm@pm.me>
2023-02-14 16:33:51 +1100

    feat: Added vite-plugin-mkcert temporarily

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import { visualizer } from "rollup-plugin-visualizer";
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
   base: '',
   server: {
+    https: true,
     port: 3000,
   },
+  plugins: [mkcert()],
   assetsInclude: ['**/*.bin', '**/*.gltf', '**/*.jpg', '**/*.hdr'],
   build: {
     target: "esnext",
